33a04db1d972a47de0e3d400aa8ad1efa9064253
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-09-04 00:04:43 -0300

    feat: add page signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,45 @@
+import type React from "react";
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useState } from "react";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
-      <Input required legend="Senha" type="password" placeholder="123456" />
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover: text-green-800 transition ease-linear"
+      >
+        Criar Conta
+      </a>
     </form>
   );
 }
