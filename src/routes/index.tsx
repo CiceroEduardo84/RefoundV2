fce550fe4c6f502750d3078c41c9f5d4934cda3b
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-09-13 23:41:31 -0300

    add: create management layout base

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./auth.route";
 import { EmployeeRoutes } from "./employee.route";
+import { ManagerRoutes } from "./manager.route";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <EmployeeRoutes />
+      <ManagerRoutes />
     </BrowserRouter>
   );
 }
